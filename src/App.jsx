b206d2a2d0d661fc98664d26a39c9b67b6324513
BexFox <karis601293@gmail.com>
2022-04-30 17:07:39 +0900

    Use MUI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function Header() {
   return (
@@ -17,12 +26,59 @@ function Nav() {
     </nav>
   );
 }
+function Article() {
+  const [open, setOpen] = useState(false);
+  return (
+    <article>
+      <h2>Welcome</h2>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit doloribus
+      consequatur officiis rem reprehenderit facere deleniti cupiditate cum non!
+      <br />
+      <ButtonGroup>
+        <Button
+          variant='outlined'
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Create
+        </Button>
+        <Button variant='outlined'>Update</Button>
+      </ButtonGroup>
+      <Button variant='outlined'>Delete</Button>
+      <Dialog open={open}>
+        <DialogTitle>Create</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Hello Create!</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant='outlined'>Create</Button>
+          <Button
+            variant='outlined'
+            onClick={() => {
+              setOpen(false);
+            }}
+          >
+            Cancel
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </article>
+  );
+}
 
 export default function App() {
   return (
-    <div>
+    <Container fixed>
       <Header></Header>
-      <Nav></Nav>
-    </div>
+      <Grid container>
+        <Grid item xs='2'>
+          <Nav></Nav>
+        </Grid>
+        <Grid item xs='10'>
+          <Article></Article>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
